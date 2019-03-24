f61aff75c481c0b156ba5efb368aad7882c43054
Jean <jean.snyman@gmial.com>
2019-03-24 07:34:45 +0200

    basic setup and code highlighting

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
+   <div id="app">
+     <vue-editor v-model="content" 
+      :editorToolbar="customToolbar" 
+      :editorOptions="editorSettings">
+    </vue-editor>
+   </div>
+ </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+ <script>
+   import { VueEditor } from 'vue2-editor'
+   import hljs from "highlight.js";
+   import "highlight.js/styles/atom-one-dark.css";
 
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
-</script>
+   export default {
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+   components: {
+      VueEditor
+   },
+
+   data() {
+       return {
+         content: '<h1>Some initial content</h1>',
+
+         customToolbar: [
+          ["bold", "italic", "underline", "strike"],
+          ["blockquote", "code-block"],
+          [{ header: 1 }, { header: 2 }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          [{ script: "sub" }, { script: "super" }],
+          [{ indent: "-1" }, { indent: "+1" }],
+          [{ direction: "rtl" }],
+          [{ size: ["small", false, "large", "huge"] }],
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          [{ font: [] }],
+          [{ color: [] }, { background: [] }],
+          [{ align: [] }],
+          ["clean"],
+          ["link", "image", "video"]
+        ],
+        editorSettings: {
+        modules: {
+          syntax: {
+            highlight: text => hljs.highlightAuto(text).value
+          }
+        }
+      }
+       }
+     }
+   }
+ </script>
